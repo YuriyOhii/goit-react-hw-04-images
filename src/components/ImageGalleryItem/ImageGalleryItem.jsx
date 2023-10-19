509dff2f1365124caae06e32924d5c33797eed1b
YuriyOhii <uaogiy@gmail.com>
2023-10-19 19:51:06 +0300

    updates ImageGalleryItem into function

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,39 +1,31 @@
-import { PureComponent } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Image } from './ImageGalleryItem.styled';
 import { Modal } from 'components/Modal';
 
-export class ImageGalleryItem extends PureComponent {
-  static propTypes = {
-    image: PropTypes.shape({
-      webformatURL: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-    }),
-  };
-  state = {
-    isModalOpen: false,
-  };
- 
-  closeModal = () => this.setState({ isModalOpen: false });
+export const ImageGalleryItem = ({
+  image: { webformatURL, largeImageURL, tags },
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  handleImageClick = () => this.setState({ isModalOpen: true });
-  render() {
-    const {
-      image: { webformatURL, largeImageURL, tags },
-    } = this.props;
-    const { isModalOpen } = this.state;
-    return (
-      <>
-        <Image onClick={this.handleImageClick} src={webformatURL} alt={tags} />
-        {isModalOpen && (
-          <Modal
-            bigImage={largeImageURL}
-            tag={tags}
-            onClick={this.closeModal}
-          />
-        )}
-      </>
-    );
-  }
-}
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleImageClick = () => setIsModalOpen(true);
+
+  return (
+    <>
+      <Image onClick={handleImageClick} src={webformatURL} alt={tags} />
+      {isModalOpen && (
+        <Modal bigImage={largeImageURL} tag={tags} onClick={closeModal} />
+      )}
+    </>
+  );
+};
+
+ImageGalleryItem.propTypes = {
+  image: PropTypes.shape({
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  }),
+};
